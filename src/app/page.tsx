93977ae47107649bb83ac4c4fc6d796f78ceed65
Nikolay Rumenov <nikolay_rumenov@starsgroup.com>
2024-01-30 14:19:40 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link'
-import {Button} from "@aws-amplify/ui-react";
 
-const categories = ["edna", "dve", "trie"]
 
 function Page() {
   return (
       <div className="bg-white">
-          <h1></h1>
-          <Link href="/login"><Button>Login</Button></Link>
+          <h1>Store</h1>
+          <br/>
+          <Link href="/login">Login</Link>
       </div>
   )
 }
